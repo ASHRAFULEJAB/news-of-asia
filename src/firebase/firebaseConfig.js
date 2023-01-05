165647cfac912aee1e478ba869a64831627118d7
Md.Ashraful Islam Khan <19103189@iubat.edu>
2023-01-05 06:07:50 +0600

    key-updated

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -5,12 +5,12 @@ import { initializeApp } from 'firebase/app'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDZ7Ht33HuYhsq2TAUMZyjHyjm3NNTjb4Y',
-  authDomain: 'news-of-asia.firebaseapp.com',
-  projectId: 'news-of-asia',
-  storageBucket: 'news-of-asia.appspot.com',
-  messagingSenderId: '315057750175',
-  appId: '1:315057750175:web:5d54a20444d1665e6062aa',
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
 }
 
 // Initialize Firebase
